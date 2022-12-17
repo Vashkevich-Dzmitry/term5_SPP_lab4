020a38812499ed87c735daf761aa14ee897dcfe9
dzmitry <dzmitry.vashkevich.a@gmail.com>
2022-12-17 09:41:55 +0300

    add pipeline add main program code

--- SPP_lab4/Program.cs
+++ SPP_lab4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SPP_lab4
+{
+    static class Program
+    {
+        static async Task Main()
+        {
+            string inputDir = "C:\\Study\\Term 5\\SPP\\SPP_lab4\\Input";
+            string outputDir = "C:\\Study\\Term 5\\SPP\\SPP_lab4\\Output";
+            int maxConcurrentInput = 3;
+            int maxConcurrentProcessing = 3;
+            int maxConcurrentOutput = 3;
+
+
+            if (Directory.Exists(inputDir) && Directory.Exists(outputDir))
+            {
+                var TestsGenerator = new TestsGenerator.TestsGeneratorPipeline(inputDir, outputDir, maxConcurrentInput, maxConcurrentProcessing, maxConcurrentOutput);
+                await TestsGenerator.Run();
+            }
+        }
+    }
+}
---
+++ TestsGenerator/TestsGeneratorPipeline.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks.Dataflow;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TestsGenerator
+{
+    public class TestsGeneratorPipeline
+    {
+        private readonly string _inputDir;
+        private readonly string _outputDir;
+        private readonly int _maxConcurrentInput;
+        private readonly int _maxConcurrentProcessing;
+        private readonly int _maxConcurrentOutput;
+
+        public TestsGeneratorPipeline(string inputDir, string outputDir, int maxConcurrentInput, int maxConcurrentProcessing, int maxConcurrentOutput)
+        {
+            _inputDir = inputDir;
+            _outputDir = outputDir;
+            _maxConcurrentInput = maxConcurrentInput;
+            _maxConcurrentProcessing = maxConcurrentProcessing;
+            _maxConcurrentOutput = maxConcurrentOutput;
+        }
+
+        public Task Run()
+        {
+            var bufferBlock = new BufferBlock<string>();
+
+            var readingBlock = new TransformBlock<string, string>(
+                async path => await GetSourceCode(path),
+                new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = _maxConcurrentInput });
+
+            var processingBlock = new TransformManyBlock<string, string>(
+                async code => await GenerateTests(code),
+                new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = _maxConcurrentProcessing });
+
+            var writingBlock = new ActionBlock<string>(
+                async tests => await WriteTests(tests),
+                new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = _maxConcurrentOutput });
+
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+
+            bufferBlock.LinkTo(readingBlock, linkOptions);
+            readingBlock.LinkTo(processingBlock, linkOptions);
+            processingBlock.LinkTo(writingBlock, linkOptions);
+
+            foreach (var file in Directory.EnumerateFiles(_inputDir))
+            {
+                bufferBlock.Post(file);
+            }
+
+            bufferBlock.Complete();
+
+            return writingBlock.Completion;
+        }
+
+        private async Task<string> GetSourceCode(string filePath)
+        {
+            return null;
+        }
+
+        private async Task<List<string>> GenerateTests(string code)
+        {
+            return null;
+        }
+
+        private async Task WriteTests(string tests)
+        {
+            return;
+        }
+    }
+}
